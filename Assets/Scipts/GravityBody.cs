b81eb087e817320c2b94d1b89586b15dd6691565
ShadoofJumper <ShadoofJumper@gmail.com>
2020-06-01 20:45:48 +0300

    Working on gravity system
    Working on gravity for obejcts.

---
+++ Assets/Scipts/GravityBody.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class GravityBody : MonoBehaviour
+{
+    [SerializeField] private float mass;
+    [SerializeField] private float radius;
+    [SerializeField] private Vector3 startVelocity;
+    [SerializeField] private bool isSun;
+
+    private Vector3 currentVelocity;
+    private Rigidbody rb;
+
+
+    private void Awake()
+    {
+        currentVelocity = startVelocity;
+        rb = GetComponent<Rigidbody>();
+        rb.mass = mass;
+    }
+
+
+    public void UpdateBodyForce(List<GravityBody> otherBodies)
+    {
+        //apply force of all bodies to this body
+        foreach (var otherBody in otherBodies)
+        {
+            if (otherBody != this)
+            {
+                Vector3 forceDir        = otherBody.transform.position - transform.position;
+                Vector3 normForceDir    = forceDir.normalized;
+                float sqrDistance       = forceDir.sqrMagnitude;
+
+                Vector3 force = normForceDir * GravitySimulator.instance.GlobalGravity * (mass * otherBody.mass) / sqrDistance;
+                Vector3 modifyForce = force + currentVelocity;
+                rb.AddForce(modifyForce);
+            }
+        }
+    }
+
+
+
+}
---
+++ Assets/Scipts/GravitySimulator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravitySimulator : MonoBehaviour
+{
+
+    #region Singlton
+
+    public static GravitySimulator instance;
+
+    private void SetupSinglton()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Debug.Log("Try create another instance of game manager!");
+        }
+
+    }
+    #endregion
+
+    [SerializeField] private float globalGravity;
+    private float physicalTimeStep = 0.01f;
+    //open for test
+    public List<GravityBody> bodies = new List<GravityBody>();
+    public float GlobalGravity => globalGravity;
+
+    private void Awake()
+    {
+        SetupSinglton();
+    }
+
+    private void FixedUpdate()
+    {
+        for (int i = 0; i < bodies.Count; i++)
+        {
+            bodies[i].UpdateBodyForce(bodies);
+        }
+    }
+
+
+}
